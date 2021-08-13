30f91dc05d0f270587f27405068ebae03fb948d8
Alex McKendrick <48360400+Hexagonsun70@users.noreply.github.com>
2021-08-13 15:41:02 +0100

    Added DataTables plugin and stylised the tables. redacted the table info

---
+++ js/tables.js
@@ -0,0 +1,11 @@
+$(document).ready( function () {
+    $('#sakila-1').DataTable();
+} );
+
+$(document).ready( function () {
+    $('#sakila-2').DataTable();
+} );
+
+$(document).ready( function () {
+    $('#sakila-3').DataTable();
+} );
